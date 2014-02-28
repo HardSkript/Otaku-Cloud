1a058fadaa39aa396b8166c75281eb58c802908d
Balthizar01 <balthizar01@live.com>
2014-02-28 00:09:05 -0800

    Fixed up code/added comments

--- forum/create_topic_parse.php
+++ forum/create_topic_parse.php
@@ -7,29 +7,38 @@ if($_SESSION['uid'] == ""){
 }
 
 
-if(isset($_POST['topic_submit'])){
-	if(($_POST['topic_title'] == "") && ($_POST['topic_content'] == "")){
-		echo "You must fill in all fields! Please return to the previous page!";
+if (isset($_POST['topic_submit'])) {
+
+	if (($_POST['topic_title'] == "") && ($_POST['topic_content'] == "")) {
+		echo "You did not fill in both fields. Please return to the previous page.";
 		exit();
-	}else{
-		include_once 'connect.php';
+	} else {
+
+		include_once("connect.php");
+
 		$cid = $_POST['cid'];
 		$title = $_POST['topic_title'];
 		$content = $_POST['topic_content'];
 		$creator = $_SESSION['uid'];
-		
-		$sql = "INSERT INTO `topics`(`category_id`, `topic_title`, `topic_creator`, `topic_date`, `topic_reply_date`) VALUES ('".$cid."', '".$title."', '".$creator."', now(), now())";
+
+		$sql = "INSERT INTO topics (category_id, topic_title, topic_creator, topic_date, topic_reply_date) VALUES ('".$cid."', '".$title."', '".$creator."', now(), now())";
+
 		$res = mysql_query($sql) or die(mysql_error());
+
 		$new_topic_id = mysql_insert_id();
-		$sql2 = "INSERT INTO posts (category_id, topic_id, post_creator, post_content, post_date) VALUES('".$cid."', '".$new_topic_id."', '".$creator."', '".$content."', now())";
+
+		$sql2 = "INSERT INTO posts (category_id, topic_id, post_creator, post_content, post_date) VALUES ('".$cid."', '".$new_topic_id."', '".$creator."', '".$content."', now())";
+
 		$res2 = mysql_query($sql2) or die(mysql_error());
+
 		$sql3 = "UPDATE categories SET last_post_date=now(), last_user_posted='".$creator."' WHERE id='".$cid."' LIMIT 1";
-		$res3= mysql_query($sql3) or die(mysql_error());
-		
-		if(($res) && ($res2) && ($res3)){
-			header("Location: index.php");
-		}else{
-			echo "There was an issue in creating the topic. Please try again!";
+
+		$res3 = mysql_query($sql3) or die(mysql_error());
+
+		if (($res) && ($res2) && ($res3)) {
+			header("Location: view_topic.php?cid=".$cid."&tid=".$new_topic_id);
+		} else {
+			echo "There was a problem creating your topic. Please try again.";
 		}
 	}
 }
--- forum/view_topic.php
+++ forum/view_topic.php
@@ -1,39 +1,66 @@
 <?php
 	require '../header.php';
+	
+	$tid = '';
 ?>
 
 <hr />
 <div id="wrapper">
 	<div id="content">
-		<?php 
-			include_once'connect.php';
-			$cid = $_GET['cid'];
-			$tid = $_GET['tid'];
-			
-			$sql = "SELECT * FROM topics WHERE category_id='".$cid."' AND id='".$tid."' LIMIT 1";
-			$res = mysql_query($sql) or die(mysql_error());
-			
-			if(mysql_num_rows($res) == 1){
-			
-				echo "<table width='100%'>";
-				if($_SESSION['uid']){
-					echo "<tr><td colspan='2'><input type='submit' value='Reply' onClick=\"window.location = post_reply.php?cid=".$cid."&tid=".$tid."\" /><hr />";
-				}else{
-					echo "<tr><td colspan='2'><p>You must be logged in to reply!</p></td></tr>";
-				}
-				
-				while($row = mysql_fetch_assoc($res)){
-					$sql2 = "SELECT * FROM posts WHERE category_id='".$cid."' AND topic_id='".$tid."'";
-					$res2 = mysql_query($sql2) or die(mysql_error());
-					
-					while($row2 = mysql_fetch_assoc($res2)){
-						echo "<tr><td valign='top' style='border: 1px solid black;'><div style='min-height: 125px;'>".$row['topic_title']."<br /> by ".$row2['post_creator']." - ".$row2['post_date']."<hr />".$row2['post_content']."</div></td><td width='200px' valign='top' align='center' style='border: 1px solid black;'>User Info Here</td></tr><tr><td colspan='2'><hr /></td></tr>";
-					}
-				}
-			}else{
-				echo "<p>Topic not found!</p>";
-			}
-		?>
+		<?php
+// Connect to the database
+include_once("connect.php");
+
+// Function that will convert a user id into their username
+function getusername($uid) {
+	$sql = "SELECT username FROM users WHERE id='".$uid."' LIMIT 1";
+	$res = mysql_query($sql) or die(mysql_error());
+	$row = mysql_fetch_assoc($res);
+	return $row['username'];
+}
+// Function that will convert the datetime string from the database into a user-friendly format
+function convertdate($date) {
+	$date = strtotime($date);
+	return date("M j, Y g:ia", $date);
+}
+
+// Assign local variables from the variables in the URL
+$cid = $_GET['cid'];
+$tid = $_GET['tid'];
+// Select the topic data depending on the $cid and $tid variables
+$sql = "SELECT * FROM topics WHERE category_id='".$cid."' AND id='".$tid."' LIMIT 1";
+// Execute the SELECT query
+$res = mysql_query($sql) or die(mysql_error());
+// Check to see if the topic exists
+if (mysql_num_rows($res) == 1) {
+	echo "<table width='100%'>";
+	// Check to see if the person accessing this page is logged in
+	if (isset($_SESSION['uid'])) { echo "<tr><td colspan='2'><input type='submit' value='Add Reply' onClick=\"window.location = 'post_reply.php?cid=".$cid."&tid=".$tid."'\" /><hr />"; } else { echo "<tr><td colspan='2'><p>Please log in to add your reply.</p><hr /></td></tr>"; }
+	// Fetch all the topic data from the database
+	while ($row = mysql_fetch_assoc($res)) {
+		// Query the posts table for all posts in the specified topic
+		$sql2 = "SELECT * FROM posts WHERE category_id='".$cid."' AND topic_id='".$tid."'";
+		// Execute the SELECT query
+		$res2 = mysql_query($sql2) or die(mysql_error());
+		// Fetch all the post data from the database
+		while ($row2 = mysql_fetch_assoc($res2)) {
+			// Echo out the topic post data from the database
+			echo "<tr><td valign='top' style='border: 1px solid #000000;'><div style='min-height: 125px;'>".$row['topic_title']."<br />by ".getusername($row2['post_creator'])." - ".convertdate($row2['post_date'])."<hr />".$row2['post_content']."</div></td><td width='200' valign='top' align='center' style='border: 1px solid #000000;'>User Info Here</td></tr><tr><td colspan='2'><hr /></td></tr>";
+		}
+		// Assign local variable for the current number of views that this topic has
+		$old_views = $row['topic_views'];
+		// Add 1 to the current value of the topic views
+		$new_views = $old_views + 1;
+		// Update query that will update the topic_views for this topic
+		$sql3 = "UPDATE topics SET topic_views='".$new_views."' WHERE category_id='".$cid."' AND id='".$tid."' LIMIT 1";
+		// Execute the UPDATE query
+		$res3 = mysql_query($sql3) or die(mysql_error());
+	}
+} else {
+	// If the topic does not exist
+	echo "<p>This topic does not exist.</p>";
+}
+?>
 	</div>
 </div>
 
